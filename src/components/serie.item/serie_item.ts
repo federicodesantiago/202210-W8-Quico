326cb1ff45be97bacb96f035fe948949bb71f524
Quico <federicodesantiago@gmail.com>
2022-11-29 18:23:19 +0100

    Commit for upload - checkpoint -

---
+++ src/components/serie.item/serie_item.ts
@@ -0,0 +1,54 @@
+import { Series } from '../../model/series.js';
+
+export class Item {
+    constructor(public serie: Series) {
+        this.createTemplate();
+    }
+
+    createTemplate() {
+        return `<li class="serie">
+                                <img
+                                    class="serie__poster"
+                                    src="${this.serie.poster}"
+                                    alt="The Sopranos poster"
+                                />
+                                <h4 class="serie__title">${this.serie.name}</h4>
+                                <p class="serie__info">${this.serie.creator} ${this.serie.year}</p>
+                                <ul class="score">
+                                    <li class="score__star">
+                                        <i
+                                            class="icon--score fas fa-star"
+                                            title="1/5"
+                                        ></i>
+                                    </li>
+                                    <li class="score__star">
+                                        <i
+                                            class="icon--score fas fa-star"
+                                            title="2/5"
+                                        ></i>
+                                    </li>
+                                    <li class="score__star">
+                                        <i
+                                            class="icon--score fas fa-star"
+                                            title="3/5"
+                                        ></i>
+                                    </li>
+                                    <li class="score__star">
+                                        <i
+                                            class="icon--score fas fa-star"
+                                            title="4/5"
+                                        ></i>
+                                    </li>
+                                    <li class="score__star">
+                                        <i
+                                            class="icon--score fas fa-star"
+                                            title="5/5"
+                                        ></i>
+                                    </li>
+                                </ul>
+                                <button class = "btnDelete">
+                                    <i class="fas fa-times-circle icon--delete"></i>
+                                </button>
+                            </li>`;
+    }
+}
--- src/mocks/series.ts
+++ src/mocks/series.ts
@@ -1,46 +1,59 @@
-import { Series } from '../model/series.js';
+import { SeriesType } from '../model/series.js';
 
-export const series = [
-    new Series(
-        'The Sopranos',
-        'David Chase',
-        1999,
-        'https://m.media-amazon.com/images/M/MV5BZGJjYzhjYTYtMDBjYy00OWU1LTg5OTYtNmYwOTZmZjE3ZDdhXkEyXkFqcGdeQXVyNTAyODkwOQ@@._V1_.jpg',
-        5,
-        21
-    ),
-    new Series(
-        'Game of Thrones',
-        'David Benioff D. B. Weiss',
-        2011,
-        'https://t1.pixers.pics/img-1fb6f67c/posters-game-of-thrones.jpg?H4sIAAAAAAAAA3VOW27EIAy8DpGS2BAIkBvs394gIjy2afNAQNtVT1_Sqp-VZXs80ngG3o9sggfrj-IT7Ktzm4ewbvXKU_J5_fJkUGOLzVTZjSBWdH74ZNMZSUcH1VLRCsbaUYpm-jRVuJv0Rl5KiXkCyEMf12f9VpfNYPcMDKkEVCC00lpYzh1aN8cuF3M4k1zH8TliH49Hi1c1f0EkYiuvACWtO6mJzupVyGt8NPCP3y-GqoL7DSgCsp-JwNnFzvcbRWS1kbNZuEELHox0izdBeoUYLBVej9qZEMIsFmmWRY7Kec_n_hu83S9APwEAAA==',
-        0,
-        164
-    ),
-    new Series(
-        'Mad Men',
-        'Matthew Weiner',
-        2007,
-        'https://pics.filmaffinity.com/Mad_Men_Serie_de_TV-351490728-large.jpg',
-        5,
-        116
-    ),
-    new Series(
-        '6 feet under',
-        'Alan Ball',
-        2001,
-        'https://www.cine.com/media/series/2711.jpg',
-        5,
-        53
-    ),
-    new Series(
-        'Atypical',
-        'Robia Rashid',
-        2017,
-        'https://www.cine.com/media/series/2711.jpg',
-        0,
-        0
-    ),
+export const series: Array<SeriesType> = [
+    {
+        id: 1,
+        name: 'The Sopranos',
+        creator: 'David Chase',
+        year: 1999,
+        poster: 'https://m.media-amazon.com/images/M/MV5BZGJjYzhjYTYtMDBjYy00OWU1LTg5OTYtNmYwOTZmZjE3ZDdhXkEyXkFqcGdeQXVyNTAyODkwOQ@@._V1_.jpg',
+        score: 5,
+        enemies: 5,
+        watched: false,
+    },
+    {
+        id: 2,
+        name: 'Game of Thrones',
+        creator: 'David Benioff D. B. Weiss',
+        year: 2011,
+        poster: 'https://t1.pixers.pics/img-1fb6f67c/posters-game-of-thrones.jpg?H4sIAAAAAAAAA3VOW27EIAy8DpGS2BAIkBvs394gIjy2afNAQNtVT1_Sqp-VZXs80ngG3o9sggfrj-IT7Ktzm4ewbvXKU_J5_fJkUGOLzVTZjSBWdH74ZNMZSUcH1VLRCsbaUYpm-jRVuJv0Rl5KiXkCyEMf12f9VpfNYPcMDKkEVCC00lpYzh1aN8cuF3M4k1zH8TliH49Hi1c1f0EkYiuvACWtO6mJzupVyGt8NPCP3y-GqoL7DSgCsp-JwNnFzvcbRWS1kbNZuEELHox0izdBeoUYLBVej9qZEMIsFmmWRY7Kec_n_hu83S9APwEAAA==',
+        score: 0,
+        enemies: 164,
+        watched: false,
+    },
+    {
+        id: 3,
+        name: 'Mad Men',
+        creator: 'Matthew Weiner',
+        year: 2007,
+        poster: 'https://pics.filmaffinity.com/Mad_Men_Serie_de_TV-351490728-large.jpg',
+        score: 5,
+        enemies: 116,
+        watched: false,
+    },
+    {
+        id: 4,
+        name: '6 feet under',
+        creator: 'Alan Ball',
+        year: 2001,
+        poster: 'https://www.cine.com/media/series/2711.jpg',
+        score: 5,
+        enemies: 53,
+        watched: false,
+    },
+    {
+        id: 5,
+        name: 'Atypical',
+        creator: 'Robia Rashid',
+        year: 2017,
+        poster: 'https://www.cine.com/media/series/2711.jpg',
+        score: 0,
+        enemies: 0,
+        watched: false,
+    },
 ];
 
 export const InitializeSeries = () => series;
+console.log('Init: ' + series);
+console.log('[0]: ' + series[0]);
+console.log('creator: ' + series[0].creator);
